bffa827ef0ddcd877fb031487aa42af772f9bdf8
luca sousa <l52980@aln.iseg.ulisboa.pt>
2021-05-11 00:01:40 +0100

    second version

--- bela.py
+++ bela.py
@@ -21,25 +21,40 @@ for div in my_divs:
     clean_div = div.prettify()
     titles.append(clean_div)
 
-for title in titles:
-    soup2 = BeautifulSoup(title, 'lxml')
-    tit = soup2.find('p', {'class': 'title'})
-    desc = soup2.find('p', {'class': 'desc'})
-    tweet = (" {} | publicado em {}. \n {}".format(tit.text, datetime, desc.text))
-    if tit not in memory_title:
-        title_content = [i for i in tit.text if i != '\n']
-        title_content = ''.join(title_content)
-        text_content = [i for i in desc.text if i != '\n']
-        text_content = ''.join(text_content)
-        content = [title_content, text_content]
-        content = ''.join(content)
-        if len(content) <= 280:
-            api.update_status('{}|{}'.format(title_content, text_content))
-        else:
-            api.update_status('{}|{}'.format(title_content, text_content[270-len(title_content)]))
-        memory_title.append(tit.text)
-        memory_text.append(desc.text)
-        break
+def get_full_tweet():
+    for title in titles:
+        soup2 = BeautifulSoup(title, 'lxml')
+        tit = soup2.find('p', {'class': 'title'})
+        desc = soup2.find('p', {'class': 'desc'})
+        if tit not in memory_title:
+            title_content = [i for i in tit.text if i != '\n']
+            title_content = ''.join(title_content)
+            text_content = [i for i in desc.text if i != '\n']
+            text_content = ''.join(text_content)
+            content = [title_content, text_content]
+            content = ''.join(content)
+            return content
+
+def update(content):
+    if len(content) <= 280:
+        api.update_status(content)
+    else:
+        first_update = content[:275]
+        second_update = content[275:]
+        api.update_status(first_update)
+        api.update_status(second_update, in_reply_to_status_id= get_last_status())
+        # memory_title.append(tit.text)
+        # memory_text.append(desc.text)
+
+def get_last_status():
+    for status in api.user_timeline():
+        return status[0]
+
+while 1:
+    tweet_content = get_full_tweet()
+    update(tweet_content)
+    # time.sleep(240)
+
 
 
 if len(memory_title) >= 10:
