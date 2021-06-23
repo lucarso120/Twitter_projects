100083779175c8ebd3091bb396a9aba244d21c8c
luca sousa <l52980@aln.iseg.ulisboa.pt>
2021-06-23 19:43:27 +0100

    bot and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# A Bot for tracking the Portuguese Parlament actions
+
+The purpose of this project is to create a twitter account that automatically updates parlamentary activity as they are published in the official website.
+For this, I did some we scrapping to retrieve data from the website, using BeautifulSoup4.
+Finally, I extract the useful information and connect via twitter API, using library Tweepy.
---
+++ parlamento_proj.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime
+from keys import consumer_key, consumer_secret, access_key, access_token
+import requests
+import tweepy
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_key)
+api = tweepy.API(auth)
+
+source = requests.get('https://www.parlamento.pt/Paginas/UltimasIniciativasEntradas.aspx').text
+soup = BeautifulSoup(source, 'lxml') 
+my_divs = soup.find_all("div", {"class": "col-xs-10 margin_padding0"})
+titles = []
+content_memory = []
+memory_title = []
+memory_text = []
+
+class getItem:
+    def __getitem__(self,i):
+        return f'Value {i}'
+
+for div in my_divs:
+    clean_div = div.prettify()
+    titles.append(clean_div)
+
+def update(content):
+    first_update = [content[:270], '...']
+    update = ''.join(first_update)  
+    api.update_status(update)
+        
+def get_last_status():
+    ids = []
+    for status in api.user_timeline('isabellacmo'):
+        ids.append(status.id)
+    return ids[0]
+
+def update_reply():
+    api.update_status('@isabellacmo Para saber mais, acesse: https://www.parlamento.pt/Paginas/UltimasIniciativasEntradas.aspx ', in_reply_to_status_id = get_last_status())
+
+import time
+
+
+for title in titles:
+    soup2 = BeautifulSoup(title, 'lxml')
+    tit = soup2.find('p', {'class': 'title'})
+    desc = soup2.find('p', {'class': 'desc'})
+    if tit not in memory_title:
+        title_content = [i for i in tit.text if i != '\n']
+        title_content = ''.join(title_content)
+        text_content = [i for i in desc.text if i != '\n']
+        text_content = ''.join(text_content)
+        content = [title_content, text_content]
+        content = '\n'.join(content)
+        memory_title.append(tit.text) # how to insert
+        memory_text.append(desc.text)
+
+
+        update(content)
+        time.sleep(5)
+        update_reply()
+        time.sleep(5)
+        if len(content_memory) >= 10:
+            del content_memory [-1]
+
